81b676a553bef78a06c08da8fd91ab57069a0d60
Maksim Laptseu <laptevm4ks@gmail.com>
2026-05-11 22:50:04 +0300

    Made 3.2.5 practice

---
+++ module_3_data_ingestion/lecture_02/homework/3.2.5/task2.py
@@ -0,0 +1,13 @@
+prices = [100, -50, 300, 40, 800]
+
+prices.remove(-50)
+prices.append(150)
+prices.sort()
+
+tax_prices = [price * 1.2 for price in prices if price * 1.2 > 100]
+
+print(f'Базовый прайс (очищенный): {prices}')
+print(f'Цены с НДС (>100): {tax_prices}')
+print(f'Общая выручка: {sum(tax_prices)}')
+print(f'Минимум: {min(tax_prices)}')
+print(f'Максимум: {max(tax_prices)}')
